429fd878e7f22724b1edfdddc1e1c1fb270e54f3
dwego <dweg0epic@outlook.com>
2023-04-26 17:22:48 -0300

    Connect MongoDB

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+  },
+  admin: {
+    type: Boolean,
+    require: true,
+  },
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
