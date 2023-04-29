266d714c20facc390386d655cd89f2978c69f344
dwego <dweg0epic@outlook.com>
2023-04-29 02:14:50 -0300

    code generator

--- src/modules/codeGenerator.js
+++ src/modules/codeGenerator.js
@@ -8,27 +8,41 @@ function generateCode() {
   return code;
 }
 
-let password = "";
-let code = generateCode();
+let codeGenerator = true;
 
-if (password == "") {
-  password == null;
+function generateRoom() {
+  let password = "1234";
+  let code = generateCode();
+
+  if (password == "") {
+    password = null;
+  }
+  if (codeGenerator) {
+    fetch("http://localhost:8080/api/code", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: code, password: password }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.log({ code: code, password: password });
+          throw new Error("Error sending code.");
+        }
+        console.log("Code sent successfully.");
+        codeGenerator = false;
+      })
+      .catch((error) => console.error(error));
+
+    const link = document.createElement("a");
+    link.href = `http://localhost:8080/rooms/${code}`;
+    link.textContent = `localhost:8080/rooms/${code}`;
+
+    const container = document.getElementById("container");
+    container.appendChild(link);
+  } else {
+    const codeResponse = document.getElementById("codeResponse");
+    codeResponse.textContent = "Room already created";
+  }
 }
-
-console.log(JSON.stringify({ code: code, password: password }));
-fetch("http://localhost:8080/api/code", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ code: code, password: password }),
-})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Error sending code.");
-    }
-    console.log("Code sent successfully.");
-  })
-  .catch((error) => console.error(error));
-
-module.exports = generateCode;
